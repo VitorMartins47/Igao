cd62ef3e3113de4eb2457481053efef86287eb8b
VitorMartins47 <vitorhavila83@gmail.com>
2023-06-26 11:58:15 -0300

    Update CarroController.js
    botao

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -11,5 +11,28 @@ module.exports ={
     },
     form:   (_,res)=> res.render('carro/form'),
 
+    edit: async(req, res) => {
+        const body = req.body;
+        const carros =  await Carro.findByPk(body.id);
+        carros.update({
+            modelo: body.Modelo,
+            marca: body.Marca,
+            valor: body.Valor
+        })
+        carros.save()
+        return res.redirect('/carros/list')
+    },
+    destroy: async(req, res) => {
+        const id = req.params.id;
+        Carro.destroy({
+            where: {id: id}
+        })
+        return res.redirect('/carros/list')
+    },
+    formEdit: async(req, res) =>{
+        const carros = await Carro.findByPk(req.params.id);
+        return res.render('carro/formEdit', {carros})
+    } 
+
     
 }
